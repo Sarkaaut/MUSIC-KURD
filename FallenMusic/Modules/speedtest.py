d053ee6177bcbe2e0729a507b3982d9f384e4528
Sarkaut Salih <111055810+Sarkaaut@users.noreply.github.com>
2023-04-28 07:48:40 +0300

    Update speedtest.py

--- FallenMusic/Modules/speedtest.py
+++ FallenMusic/Modules/speedtest.py
@@ -32,35 +32,35 @@ def testspeed(m):
     try:
         test = speedtest.Speedtest()
         test.get_best_server()
-        m = m.edit("**⇆ جارٍ تشغيل اختبار سرعة التنزيل...**")
+        m = m.edit("**⇆ ڕاکردنی تاقیکردنەوەی خێرایی دابەزاندن...**")
         test.download()
-        m = m.edit("**⇆ تشغيل اختبار سرعة التحميل...**")
+        m = m.edit("**⇆ تاقیکردنەوەی خێرایی دابەزاندن ئەنجام بدە...**")
         test.upload()
         test.results.share()
         result = test.results.dict()
-        m = m.edit("**↻ مشاركة نتائج اختبار السرعة...**")
+        m = m.edit("**↻ ئەنجامی تاقیکردنەوەی خێرایی هاوبەش بکە...**")
     except Exception as e:
         return m.edit(e)
     return result
 
 
-@app.on_message(filters.command(["speedtest", "spt"]) | filters.command(["فحص","السرعة","السرعه","سرعه","سرعة"],prefixes= ["/", "!","","#"]) & SUDOERS)
+@app.on_message(filters.command(["speedtest", "spt"]) | filters.command([" تاقیکردنەوە","xiray","خيراي","سە ریعی","خێرایی"],prefixes= ["/", "!","","#"]) & SUDOERS)
 async def speedtest_function(_, message):
-    m = await message.reply_text("**⎊ تشغيل اختبار السرعة...**")
+    m = await message.reply_text("**⎊ تاقیکردنەوەی خێرایی ئەنجام بدە...**")
     loop = asyncio.get_event_loop()
     result = await loop.run_in_executor(None, testspeed, m)
-    output = f"""✯ **نتائج اختبار سرعه سبايدر ** ✯
+    output = f"""✯ **ئەنجامی تاقیکردنەوەی خێرایی بۆت میوزیک ** ✯
     
-<u>**⎊ عميل :**</u>
-**⎊ مزود خدمة الإنترنت :** {result['client']['isp']}
+<u>**⎊ کلایەنت :**</u>
+**⎊ دابینکه‌ری خزمه‌تگوزاری ئینته‌رنێت :** {result['client']['isp']}
 **⎊ الدولة :** {result['client']['country']}
   
-<u>**⎊ سيرفر :**</u>
-**⎊ الاسم :** {result['server']['name']}
-**⎊ الدولة :** {result['server']['country']}, {result['server']['cc']}
-**⎊ راعي :** {result['server']['sponsor']}
-**⎊ وقت الإستجابة :** {result['server']['latency']}  
-**⎊ البنج :** {result['ping']}"""
+<u>**⎊ سێرڤەر :**</u>
+**⎊ ناوەکە :** {result['server']['name']}
+**⎊ وڵات :** {result['server']['country']}, {result['server']['cc']}
+**⎊ رام :** {result['server']['sponsor']}
+**⎊ کاتی وەڵامدانەوە :** {result['server']['latency']}  
+**⎊ پینگ :** {result['ping']}"""
     msg = await app.send_photo(
         chat_id=message.chat.id, photo=result["share"], caption=output
     )
