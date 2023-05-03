31f9ff96ad9d783ab9d6a92231b76fab4d5d3255
Sarkaut Salih <111055810+Sarkaaut@users.noreply.github.com>
2023-05-03 20:29:36 +0300

    Update song.py

--- FallenMusic/Modules/song.py
+++ FallenMusic/Modules/song.py
@@ -58,13 +58,13 @@ async def song(_, message: Message):
             f"شکستی هێنا لە وەرگرتنی ڕێگاکە لە \n\n**هوکار :** `{ex}`"
         )
 
-    await m.edit_text("⎊ لە دابە زین دایە چاوەڕێ بکە,\n\n⎊ لە لایە ن ‌SPIDER..")
+    await m.edit_text("⎊ لە دابە زین دایە چاوەڕێ بکە,\n\n⎊ لە لایە ن ‌Bot Music...")
     try:
         with yt_dlp.YoutubeDL(ydl_opts) as ydl:
             info_dict = ydl.extract_info(link, download=False)
             audio_file = ydl.prepare_filename(info_dict)
             ydl.process_info(info_dict)
-        rep = f"⎊ **العنوان :** [{title[:23]}]({link})\n⎊ **ماوە :** `{duration}`\n⎊ ** لە لایە ن :** {BOT_MENTION}"
+        rep = f"⎊ **ناونیشانەکە :** [{title[:23]}]({link})\n⎊ **ماوە :** `{duration}`\n⎊ ** لە لایە ن :** {BOT_MENTION}"
         secmul, dur, dur_arr = 1, 0, duration.split(":")
         for i in range(len(dur_arr) - 1, -1, -1):
             dur += int(dur_arr[i]) * secmul
